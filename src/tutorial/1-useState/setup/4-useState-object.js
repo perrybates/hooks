0bceeb435d3476f8f9436e5252efef0d54a291fa
Perry Bates <perryboateng08@gmail.com>
2021-01-18 02:46:24 +0000

    covered up to useEffect with a basic example setup

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,34 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+ 
+  const [person, setPerson] = useState({
+    name:'peter', 
+    age :24, 
+    message:'beautiful message'
+  })
+
+  // const [name, setName] = useState('james')
+  // const [age, setAge] = useState('24')
+  // const [message, setMessage] = useState('molly is the new company')
+
+  const changeMessage= () => {
+   setPerson({ ...person, message:'hello world'})
+  //  setMessage('tesla is a big company')
+  }
+
+  return (
+    <>
+    <h3>{person.name}</h3>
+    <h3>{person.age}</h3>
+    <h3>{person.message}</h3>
+    <button className="btn" onClick={changeMessage}>
+      change message
+    </button>
+    
+    </>
+  )
+  
 };
 
 export default UseStateObject;
